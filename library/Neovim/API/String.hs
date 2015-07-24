8719f0ce3e02aea3d076f37019fda138a7dde5e3
Sebastian Witte <woozletoff@gmail.com>
2015-07-24 12:14:39 +0200

    Add warning to Neovim.API.String about generated code

--- library/Neovim/API/String.hs
+++ library/Neovim/API/String.hs
@@ -10,6 +10,10 @@ License     :  Apache-2.0
 Maintainer  :  woozletoff@gmail.com
 Stability   :  experimental
 
+Note that this module is completely generated. If you're reading this on
+hackage, the actual functions of this module may be different from what is
+available to you. All the functions in this module depend on the neovim version
+that was used when this package was compiled.
 -}
 module Neovim.API.String
     where
