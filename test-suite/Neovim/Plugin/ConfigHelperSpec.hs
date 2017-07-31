6381a812ebd2eb953d5cbf50d37ef7a9b6fa9989
Sebastian Witte <woozletoff@gmail.com>
2017-07-31 19:03:27 +0200

    Fix encoding issues when tests are not run on a machen with UTF8 as the default

--- test-suite/Neovim/Plugin/ConfigHelperSpec.hs
+++ test-suite/Neovim/Plugin/ConfigHelperSpec.hs
@@ -9,6 +9,9 @@ import           Neovim.Quickfix
 
 import           Text.Megaparsec
 
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.UTF8 as BS
+
 isLeft :: Either a b -> Bool
 isLeft (Left _) = True
 isLeft _        = False
@@ -16,11 +19,15 @@ isLeft _        = False
 isRight :: Either a b -> Bool
 isRight = not . isLeft
 
+
+readFileUTF8 :: FilePath -> IO String
+readFileUTF8 = fmap BS.toString . BS.readFile
+
 spec :: Spec
 spec = do
   describe "parseQuickFfixItems" $ do
     it "should match this test file 1" $ do
-      e <- readFile "./test-files/compile-error-for-quickfix-test1"
+      e <- readFileUTF8 "./test-files/compile-error-for-quickfix-test1"
       let [qs] = parseQuickfixItems e
       bufOrFile qs `shouldBe` Right "/.config/nvim/nvim.hs"
       lnumOrPattern qs `shouldBe` Left 25
@@ -29,7 +36,7 @@ spec = do
       errorType qs `shouldBe` Error
 
     it "should match this test file 2" $ do
-      e <- readFile "./test-files/compile-error-for-quickfix-test2"
+      e <- readFileUTF8 "./test-files/compile-error-for-quickfix-test2"
       let [qs] = parseQuickfixItems e
       bufOrFile qs `shouldBe` Right "/.config/nvim/lib/TestPlugins.hs"
       lnumOrPattern qs `shouldBe` Left 11
@@ -38,7 +45,7 @@ spec = do
       errorType qs `shouldBe` Error
 
     it "should match this test file 3" $ do
-      e <- readFile "./test-files/compile-error-for-quickfix-test3"
+      e <- readFileUTF8 "./test-files/compile-error-for-quickfix-test3"
       let [qs] = parseQuickfixItems e
       bufOrFile qs `shouldBe` Right "/.config/nvim/nvim.hs"
       lnumOrPattern qs `shouldBe` Left 25
@@ -47,7 +54,7 @@ spec = do
       errorType qs `shouldBe` Error
 
     it "should match this test file 4" $ do
-      e <- readFile "./test-files/compile-error-for-quickfix-test4"
+      e <- readFileUTF8 "./test-files/compile-error-for-quickfix-test4"
       let [qs] = parseQuickfixItems e
       bufOrFile qs `shouldBe` Right "/.config/nvim/lib/TestPlugins.hs"
       lnumOrPattern qs `shouldBe` Left 23
@@ -56,7 +63,7 @@ spec = do
       errorType qs `shouldBe` Error
 
     it "should match this test file 5" $ do
-      e <- readFile "./test-files/compile-error-for-quickfix-test5"
+      e <- readFileUTF8 "./test-files/compile-error-for-quickfix-test5"
       let [q1,q2] = parseQuickfixItems e
       bufOrFile q1 `shouldBe` Right "/.config/nvim/lib/TestPlugins.hs"
       bufOrFile q2 `shouldBe` Right "/.config/nvim/lib/TestPlugins.hs"
@@ -70,7 +77,7 @@ spec = do
       errorType q2 `shouldBe` Warning
 
     it "should match this test file 6" $ do
-      e <- readFile "./test-files/compile-error-for-quickfix-test6"
+      e <- readFileUTF8 "./test-files/compile-error-for-quickfix-test6"
       let [q1] = parseQuickfixItems e
       bufOrFile q1 `shouldBe` Right "/home/test/.config/nvim/nvim.hs"
       lnumOrPattern q1 `shouldBe` Left 8
@@ -79,9 +86,9 @@ spec = do
       errorType q1 `shouldBe` Error
 
     it "should parse all files concatenated" $ do
-      e1 <- readFile "./test-files/compile-error-for-quickfix-test1"
-      e2 <- readFile "./test-files/compile-error-for-quickfix-test2"
-      e3 <- readFile "./test-files/compile-error-for-quickfix-test3"
+      e1 <- readFileUTF8 "./test-files/compile-error-for-quickfix-test1"
+      e2 <- readFileUTF8 "./test-files/compile-error-for-quickfix-test2"
+      e3 <- readFileUTF8 "./test-files/compile-error-for-quickfix-test3"
       let qs = parseQuickfixItems $ unlines [e1,e2,e3]
       length qs `shouldBe` 3
       case qs of
