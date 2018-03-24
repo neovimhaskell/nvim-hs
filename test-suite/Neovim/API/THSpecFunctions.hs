e2915744d86a673f538a02b3f99e211b08028c33
Sebastian Witte <woozletoff@gmail.com>
2018-03-24 13:33:27 +0100

    Fix test functions

--- test-suite/Neovim/API/THSpecFunctions.hs
+++ test-suite/Neovim/API/THSpecFunctions.hs
@@ -4,19 +4,19 @@ module Neovim.API.THSpecFunctions
 import qualified Data.Map as Map
 import           Neovim
 
-testFunction0 :: Neovim' Int
+testFunction0 :: Neovim env Int
 testFunction0 = return 42
 
-testFunction2 :: CommandArguments -> String -> [String] -> Neovim' Double
+testFunction2 :: CommandArguments -> String -> [String] -> Neovim env Double
 testFunction2 _ _ _ = return 2
 
-testFunctionMap :: Map.Map String Int -> String -> Neovim' (Maybe Int)
+testFunctionMap :: Map.Map String Int -> String -> Neovim env (Maybe Int)
 testFunctionMap m k = return $ Map.lookup k m
 
 testSucc :: Int -> Neovim env Int
 testSucc = return . succ
 
-testCommandOptArgument :: CommandArguments -> Maybe String -> Neovim' String
+testCommandOptArgument :: CommandArguments -> Maybe String -> Neovim env String
 testCommandOptArgument _ ms = case ms of
     Just x  -> return x
     Nothing -> return "default"
